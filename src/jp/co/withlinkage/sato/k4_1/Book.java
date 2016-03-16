b4c68a48f1800b765b6e93524fab173469cf5d8a
masa2206 <satoh.masa176@gmail.com>
2016-03-16 09:26:19 +0900

    ssss

---
+++ src/jp/co/withlinkage/sato/k4_1/Book.java
@@ -0,0 +1,44 @@
+package jp.co.withlinkage.sato.k4_1;
+import java.util.Date;
+
+public class Book implements Comparable<Book>, Cloneable {
+	private String title;
+	private Date publishDate;
+	private String comment;
+	
+	
+	public boolean equals(Object o){
+		if(o == this) return true;
+		if(o == null) return false;
+		if(!(o instanceof Book)) return false;
+		Book r = (Book) o;
+		if(!(this.title.trim().equals(r.title.trim()))){
+			return false;
+		}
+		if(!(this.publishDate.equals(r.publishDate))){
+			return false;
+		}
+		return true;
+	}
+	
+	public int hashCode(){
+		int result = 37;
+		result = result * 31 + title.hashCode();
+		result = result * 31 + publishDate.hashCode();
+		return result;
+	}
+	
+	public int compareTo(Book o){
+		return this.publishDate.compareTo(o.publishDate);
+	}
+	
+	public Book clone(){
+		Book result = new Book();
+		result.title = this.title;
+		// ↓ publishDateにcloneを付ける理由とは?
+		// ↓ (Date)によるキャスト化の理由とは？
+		result.publishDate = (Date) this.publishDate.clone();
+		result.comment = this.comment;
+		return result;
+	}
+}

facfba6157f6ade7e500a780b506637ce617baba
ny2528 <naoky25@gmail.com>
2016-03-17 22:10:55 +0900

    Book.javaのコメントにコメント返し

--- src/jp/co/withlinkage/sato/k4_1/Book.java
+++ src/jp/co/withlinkage/sato/k4_1/Book.java
@@ -5,8 +5,8 @@ public class Book implements Comparable<Book>, Cloneable {
 	private String title;
 	private Date publishDate;
 	private String comment;
-	
-	
+
+
 	public boolean equals(Object o){
 		if(o == this) return true;
 		if(o == null) return false;
@@ -20,23 +20,25 @@ public class Book implements Comparable<Book>, Cloneable {
 		}
 		return true;
 	}
-	
+
 	public int hashCode(){
 		int result = 37;
 		result = result * 31 + title.hashCode();
 		result = result * 31 + publishDate.hashCode();
 		return result;
 	}
-	
+
 	public int compareTo(Book o){
 		return this.publishDate.compareTo(o.publishDate);
 	}
-	
+
 	public Book clone(){
 		Book result = new Book();
 		result.title = this.title;
 		// ↓ publishDateにcloneを付ける理由とは?
+		//TODO: 同じ参照を指すから: this.publishDate#setXX()がcloneのそれにも反映されてしまう。（古いメソッドだが）
 		// ↓ (Date)によるキャスト化の理由とは？
+		//TODO: #clone() の戻り値がObjectだから
 		result.publishDate = (Date) this.publishDate.clone();
 		result.comment = this.comment;
 		return result;
